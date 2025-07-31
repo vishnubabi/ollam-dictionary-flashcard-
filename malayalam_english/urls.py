5092650f742ef67d1fa029402551bdc76d320abe
BABI <thevishnubabi@gmail.com>
2025-07-31 17:17:42 +0530

    Update dictionary_list.html

--- malayalam_english/urls.py
+++ malayalam_english/urls.py
@@ -20,4 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('dictionary_app.urls')),
+    path('flashcards/', include('flashcard_app.urls'))
 ]
